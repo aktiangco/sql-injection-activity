a1553e2b99140f009b66ad30b7dd669105b23490
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-04-20 22:26:18 -0700

    app.js / post routes

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
-const sqlite3 = require('sqlite3').verbose();
-
+//* Dependencies
 const
     http = require('http'),
     path = require('path'),
     express = require('express'),
     bodyParser = require('body-parser');
-    
+ 
+const sqlite3 = require('sqlite3').verbose();
 
+//* Middleware
 const app = express();
 app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+//! Database SQL
 const db = new sqlite3.Database(':memory:');
 db.serialize(function () {
  db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
  db.run("INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')");
 });
 
+//* ROOT
 app.get('/', (req, res) => {
     res.sendFile('index.html')
 });
 
+app.post('/login', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    let query = "SELECT title FROM user where username = '" + username + "' and password = '" + password + "'";
+
+    console.log("username: " + username);
+	console.log("password: " + password);
+    console.log('query: ' + query);
+    
+    
+})
+
+app.listen(3000)
