a14785a5f8b083ef124a5a81797ece808950a76a
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-04-20 21:46:05 -0700

    working on app.js / working on route

---
+++ app.js
@@ -0,0 +1,24 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const
+    http = require('http'),
+    path = require('path'),
+    express = require('express'),
+    bodyParser = require('body-parser');
+    
+
+const app = express();
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+const db = new sqlite3.Database(':memory:');
+db.serialize(function () {
+ db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
+ db.run("INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')");
+});
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html')
+});
+
