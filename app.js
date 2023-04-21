603025a6a8a39815c6695455ab7b3de41f82caa3
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-04-20 22:41:07 -0700

    completed

--- app.js
+++ app.js
@@ -25,16 +25,31 @@ app.get('/', (req, res) => {
     res.sendFile('index.html')
 });
 
+//* Routes
 app.post('/login', (req, res) => {
     let username = req.body.username;
     let password = req.body.password;
     let query = "SELECT title FROM user where username = '" + username + "' and password = '" + password + "'";
-
+    //! To view SQL injection
     console.log("username: " + username);
 	console.log("password: " + password);
     console.log('query: ' + query);
     
-    
-})
+    //* To Verify log in and Handle Errors
+    db.get(query, function (err, row) {
+		if (err) {
+			console.log('ERROR', err);
+			res.redirect("/index.html#error");
+		} else if (!row) {
+			res.redirect("/index.html#unauthorized");
+		} else {
+			res.send('Hello <b>' + row.title + '!</b><br /> This file contains all your secret data: <br /><br /> SECRETS <br /><br /> MORE SECRETS <br /><br /> <a href="/index.html">Go back to login</a>');
+		}
+	});
+});  
 
+//! SQL injection in the password to access all you database
+//! " unknown' OR '1'='1 "
+
+//* Listen to a PORT
 app.listen(3000)
